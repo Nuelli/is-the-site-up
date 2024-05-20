59749d06f02285b93c59b30926573e703d327b1d
Nuelli <emmanuelabwao@gmail.com>
2024-05-20 11:58:05 +0300

    first commit

---
+++ src/main/java/io/checker/isthesiteup/IsTheSiteUp.java
@@ -0,0 +1,13 @@
+package io.checker.isthesiteup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsTheSiteUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsTheSiteUp.class, args);
+	}
+
+}
